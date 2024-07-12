2890416597948a5aee6cbdf72300469a541b454a
Payal1806dev <payalbiz4group@gmail.com>
2024-07-12 18:52:15 +0530

    Update Login.tsx

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -9,7 +9,7 @@ import {
 } from "@react-oauth/google";
 import { LoginAction } from "../stateManagement/actions/authAction";
 import { IUserAuthData } from "../interfaces/authInterface";
-import "./Login.css"; // Import your CSS file
+import "./Login.css";
 
 type FormValues = {
   id: number;
@@ -20,6 +20,8 @@ type FormValues = {
 };
 
 const Login: React.FC = () => {
+  const clientId =
+    "763033723945-4jf16tnnn3ta5p82k0auh75o1n5o09du.apps.googleusercontent.com";
   const {
     register,
     handleSubmit,
@@ -42,7 +44,7 @@ const Login: React.FC = () => {
           id: response.data.data.id,
           email: response.data.data.email,
           token: response.data.data.token,
-          first_name: response.data.data.first_name
+          first_name: response.data.data.first_name,
         };
         console.log("Server Response:", user);
         dispatch(LoginAction(user));
@@ -96,7 +98,7 @@ const Login: React.FC = () => {
         </div>
         <button type="submit">Login</button>
         <div className="google-login-container">
-          <GoogleOAuthProvider clientId="your-google-client-id">
+          <GoogleOAuthProvider clientId={clientId}>
             <GoogleLogin
               onSuccess={handleSuccess}
               onError={() => console.log("Google sign-in failed")}
@@ -109,207 +111,3 @@ const Login: React.FC = () => {
 };
 
 export default Login;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import { useForm, SubmitHandler } from "react-hook-form";
-// import axios from "axios";
-// import { useDispatch } from "react-redux";
-// import {
-//   GoogleOAuthProvider,
-//   GoogleLogin,
-//   CredentialResponse,
-// } from "@react-oauth/google";
-// import { LoginAction } from "../stateManagement/actions/authAction";
-// import { IUserAuthData } from "../interfaces/authInterface";
-
-// type FormValues = {
-//   id: number;
-//   // first_name: string;
-//   // last_name: string;
-//   email: string;
-
-//   token: string;
-//   password: string;
-// };
-
-// const Login: React.FC = () => {
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors },
-//   } = useForm<FormValues>();
-
-//   const dispatch = useDispatch();
-
-//   const onSubmit: SubmitHandler<FormValues> = async (data) => {
-//     try {
-//       const response = await axios.post(
-//         "http://localhost:5000/api/admin/admin-login",
-//         {
-//           email: data.email,
-//           password: data.password,
-//         }
-//       );
-//       if (response.data) {
-//         const user: IUserAuthData = {
-//           id: response.data.data.id,
-//           // first_name: response.data.data.first_name,
-//           // last_name: response.data.data.last_name,
-//           email: response.data.data.email,
-
-//           token: response.data.data.token,
-//           // password: password // Optionally store password if needed (generally not recommended)
-//         };
-//         console.log("Server Response:", user);
-//         dispatch(LoginAction(user));
-//         console.log("Login successful:", response.data);
-//       }
-//     } catch (error: any) {
-//       console.error("Login failed:", error);
-//       if (error.response) {
-//         console.error("Backend error:", error.response.data);
-//       }
-//     }
-//   };
-
-//   const handleSuccess = async (credentialResponse: CredentialResponse) => {
-//     try {
-//       const res = await axios.post(
-//         "http://localhost:5000/api/auth/google-login",
-//         {
-//           token: credentialResponse.credential,
-//         }
-//       );
-//       dispatch(LoginAction(res.data.token));
-//       console.log("Google login successful:", res.data);
-//       // Optionally, redirect user based on response
-//     } catch (error: any) {
-//       console.error("Google login failed:", error);
-//       if (error.response) {
-//         console.error("Backend error:", error.response.data);
-//       }
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//       <h2>Login</h2>
-//       <div>
-//         <label>Email:</label>
-//         <input
-//           type="email"
-//           {...register("email", { required: "Email is required" })}
-//         />
-//         {errors.email && <p>{errors.email.message}</p>}
-//       </div>
-//       <div>
-//         <label>Password:</label>
-//         <input
-//           type="password"
-//           {...register("password", { required: "Password is required" })}
-//         />
-//         {errors.password && <p>{errors.password.message}</p>}
-//       </div>
-
-//       <button type="submit">Login</button>
-//       <GoogleOAuthProvider clientId="your-google-client-id">
-//         <GoogleLogin
-//           onSuccess={handleSuccess}
-//           onError={() => console.log("Google sign-in failed")}
-//         />
-//       </GoogleOAuthProvider>
-//     </form>
-//   );
-// };
-
-// export default Login;
-
